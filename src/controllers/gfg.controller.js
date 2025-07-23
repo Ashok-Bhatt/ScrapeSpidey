fe7d7c78ba92985e6f971bc5a55782bad1a36e2a
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-07-23 12:54:58 +0530

    Remove chromium executable path

--- src/controllers/gfg.controller.js
+++ src/controllers/gfg.controller.js
@@ -16,7 +16,6 @@ const getUserInfo = async (req, res) => {
         browser = await puppeteer.launch({
             headless: true,
             args: ['--no-sandbox', '--disable-gpu', "--disable-setuid-sandbox", "--single-process", "--no-zygote"],
-            executablePath: PUPPETEER_EXECUTABLE_PATH,
         });
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
