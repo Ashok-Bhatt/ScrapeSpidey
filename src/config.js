b474ecdccfec502b9a8d350e4d4dacbbb275f516
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-07-23 11:37:31 +0530

    Add environment variable setup for frontend and backend

--- src/config.js
+++ src/config.js
@@ -1,11 +1,15 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import puppeteer from 'puppeteer';
 
 const PORT = process.env.PORT || 8000;
-const NODE_ENV = "production"
-const PUPPETEER_EXECUTABLE_PATH = NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath();
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+const PUPPETEER_EXECUTABLE_PATH = NODE_ENV === "production"
+  ? process.env.PUPPETEER_EXECUTABLE_PATH
+  : puppeteer.executablePath();
 
 export {
-    PORT,
-    NODE_ENV,
-    PUPPETEER_EXECUTABLE_PATH,
-}
+  PORT,
+  NODE_ENV,
+  PUPPETEER_EXECUTABLE_PATH,
+};
