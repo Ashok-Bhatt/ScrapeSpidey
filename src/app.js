4506e1dbd4a720bb74b951ed1d3244297c5c3cc9
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-07-23 10:35:32 +0530

    Add Dockerfile

--- src/app.js
+++ src/app.js
@@ -6,4 +6,6 @@ const app = express();
 app.use(cors({origin : "*",}));
 app.use("/api/v1/gfg", GfgRouter);
 
+app.get("/").send("Welcome to scrape spidey! Scrape data from your favorite websites.")
+
 export {app};
