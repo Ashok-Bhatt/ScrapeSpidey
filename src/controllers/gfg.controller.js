d139710d5c901b7d6b118b7c1ef2c035ed872f3f
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-09-16 19:33:22 +0530

    Resolve bugs and improve code quality in gfg controller

--- src/controllers/gfg.controller.js
+++ src/controllers/gfg.controller.js
@@ -2,82 +2,116 @@ import {configChromeDriver} from "../utils/chromeDriver.js"
 
 const getUserInfo = async (req, res) => {
     const username = req.params.user;
+    const includeContests = req.query.includeContests || false;
     const url = `https://www.geeksforgeeks.org/user/${username}/`;
 
-    if (!username) {
-        return res.status(400).json({message : "Username not found"});
-    }
+    if (!username) return res.status(400).json({message : "Username not found"});
 
     let browser;
     let page;
     try {
         browser = await configChromeDriver();
 
-        if (!browser) {
-            return res.status(500).json({ error: "Failed to setup browser" });
-        }
+        if (!browser) return res.status(500).json({ error: "Failed to setup browser" });
 
         page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
         await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 2 * 60 * 1000 });
         await page.waitForSelector('.educationDetails_head_left--text__tgi9I', { timeout: 5000 });
 
-        const data = await page.evaluate(() => {
+        const data = await page.evaluate((includeContests) => {
+
             const getText = (element) => element?.textContent || "NA";
+            const getTitleCase = (word) => word.charAt(0).toUpperCase() + word.slice(1);
 
-            const username = getText(document.querySelector(".profilePicSection_head_userHandle__oOfFy"));
-            const avatar = document.querySelector(".profilePicSection_head_img__1GLm0 > span > img")?.getAttribute("src") || "NA";
-            const institutionName = getText(document.querySelector(".educationDetails_head_left--text__tgi9I"));
-            const institutionRank = getText(document.querySelector(".educationDetails_head_left_userRankContainer--text__wt81s b"))?.split(" ")[0];
-            const languagesUsed = getText(document.querySelector(".educationDetails_head_right--text__lLOHI"));
-            const userMaxStreak = Array.from(document.querySelector(".circularProgressBar_head_mid_streakCnt__MFOF1").childNodes).filter(node => node.nodeType === Node.TEXT_NODE).map(node => node.textContent.trim()).join(' ').trim();
-            let globalMaxStreak = getText(document.querySelector(".circularProgressBar_head_mid_streakCnt--glbLongStreak__viuBP")) || "NA";
-            const contestRating = getText(document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[0]);
-            const contestLevel = getText(document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[1]);
-            const contestRanking = getText(document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[2]);
-            const contestAttended = getText(document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[3]);
-            let contestTopPercentage = document.querySelector(".contestDetailsCard_head__0MvGa > p")?.textContent.split(" ")[3] || "NA";
-            const contestTotalParticipants = document.querySelector(".contestDetailsCard_head__0MvGa > p")?.textContent.split(" ")[5] || "NA";
-            let codingScore = getText(document.querySelectorAll(".scoreCard_head_left--score__oSi_x")[0]);
-            const totalProblemsSolved = getText(document.querySelectorAll(".scoreCard_head_left--score__oSi_x")[1]);
+            const difficultyTags = ["School", "Basic", "Easy", "Medium", "Hard"];
 
-            if (globalMaxStreak !== "NA") globalMaxStreak = globalMaxStreak.slice(1);
-            if (contestTopPercentage !== "NA") contestTopPercentage = contestTopPercentage.slice(0, contestTopPercentage.length - 1);
-            if (codingScore === "__") codingScore = "0";
+            const usernameElement = document.querySelector(".profilePicSection_head_userHandle__oOfFy");
+            const avatarElement = document.querySelector(".profilePicSection_head_img__1GLm0 > span > img");
+            const institutionNameElement = document.querySelector(".educationDetails_head_left--text__tgi9I");
+            const institutionRankElement = document.querySelector(".educationDetails_head_left_userRankContainer--text__wt81s b");
+            const languagesUsedElement = document.querySelector(".educationDetails_head_right--text__lLOHI");
+            const maxStreakElement = document.querySelector(".circularProgressBar_head_mid_streakCnt__MFOF1");
+            const codingScoreElement = document.querySelectorAll(".scoreCard_head_left--score__oSi_x")[0];
+            const totalProblemsSolvedElement = document.querySelectorAll(".scoreCard_head_left--score__oSi_x")[1];
+
+            const username = getText(usernameElement);
+            const avatar = avatarElement?.getAttribute("src") || "NA";
+            const institutionName = getText(institutionNameElement);
+            const institutionRank = getText(institutionRankElement);
+            const languagesUsed = getText(languagesUsedElement);
+            const maxStreak = getText(maxStreakElement);
+            let codingScore = getText(codingScoreElement);
+            const totalProblemsSolved = getText(totalProblemsSolvedElement);
+
+            if (codingScore == "__" || codingScore == "NA") codingScore = "0";
             const avatarUrl = avatar !== "NA" ? "https://www.geeksforgeeks.org/" + avatar : "NA";
-
             const problemsSolved = {
-                total: totalProblemsSolved === "__" ? "0" : totalProblemsSolved,
+                Total: (totalProblemsSolved === "__" || totalProblemsSolved === "NA") ? 0 : parseInt(totalProblemsSolved),
             };
 
+            // Extracting problems count for different difficulty level
             document.querySelectorAll(".problemNavbar_head_nav--text__UaGCx").forEach((selector) => {
-                const key = selector.textContent.split(" ")[0].toLocaleLowerCase();
+                const key = getTitleCase(selector.textContent.split(" ")[0].toLowerCase());
                 let value = selector.textContent.split(" ")[1];
                 value = value.slice(1, value.length - 1);
-                problemsSolved[key] = value;
+                problemsSolved[key] = parseInt(value);
             });
 
-            return {
+            // If User has not solved a single question, then it will initialize the number of questions to each difficulty level to 0
+            for (let i=0; i<difficultyTags.length; i++){
+                if (!problemsSolved[difficultyTags[i]]){
+                    problemsSolved[difficultyTags[i]] = 0
+                }
+            }
+
+            const response = {
                 username : username,
                 avatar: avatarUrl,
                 institutionName : institutionName,
-                institutionRank : institutionRank,
-                languagesUsed : languagesUsed,
-                userMaxStreak : userMaxStreak,
-                globalMaxStreak : globalMaxStreak,
-                contestRating : contestRating,
-                contestLevel : contestLevel,
-                contestRanking : contestRanking,
-                contestAttended : contestAttended,
-                contestTopPercentage : contestTopPercentage,
-                contestTotalParticipants : contestTotalParticipants,
-                codingScore : codingScore,
+                institutionRank : institutionRank=="NA" ? "NA" : parseInt(institutionRank.split(" ")[0]),
+                languagesUsed : languagesUsed=="NA" ? [] : languagesUsed.split(",").map((language)=>language.trim()),
+                userMaxStreak : maxStreak=="NA" ? 0 : parseInt(maxStreak.split("/")[0]),
+                globalMaxStreak : maxStreak=="NA" ? 0 : parseInt(maxStreak.split("/")[1]),
+                codingScore : codingScore=="NA" ? 0 : parseInt(codingScore),
                 problemsSolved : problemsSolved,
             };
-        });
+
+            if (includeContests==="true"){
+                const contestRatingElement = document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[0];
+                const contestLevelElement = document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[1];
+                const contestRankingElement = document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[2];
+                const contestAttendedElement = document.querySelectorAll(".contestDetailsCard_head_detail--text__NG_ae")[3];
+                const contestTopPercentageElement =  document.querySelector(".contestDetailsCard_head__0MvGa > p");
+                const contestTotalParticipantsElement = document.querySelector(".contestDetailsCard_head__0MvGa > p");
+
+                const contestRating = getText(contestRatingElement);
+                const contestLevel = getText(contestLevelElement);
+                const contestRanking = getText(contestRankingElement);
+                const contestAttended = getText(contestAttendedElement);
+                let contestTopPercentage = getText(contestTopPercentageElement);
+                const contestTotalParticipants = getText(contestTotalParticipantsElement);
+
+                if (contestTopPercentage !== "NA") contestTopPercentage = contestTopPercentage.split(" ")[3].slice(0, contestTopPercentage.length - 1);
+
+                // Extracting the contest data
+                const contestData = {
+                    contestRating : contestRating=="NA" ? 1500 : parseInt(contestRating),
+                    contestLevel : contestLevel=="NA" ? 0 : parseInt(contestLevel),
+                    contestRanking : contestRanking=="NA" ? "NA" : parseInt(contestRanking),
+                    contestAttended : contestAttended=="NA" ? 0 : parseInt(contestAttended),
+                    contestTopPercentage: contestTopPercentage=="NA" ? "NA" : parseInt(contestTopPercentage),
+                    contestTotalParticipants: contestTotalParticipants=="NA" ? "NA" : parseInt(contestTotalParticipants.split(" ")[5]),
+                }
+
+                response.contestData = contestData;
+            }
+
+            return response;
+            
+        }, includeContests);
         
         return res.status(200).json(data);
-        browser.close();
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({ error: "Failed to fetch data", details: error.message });
